10416122f75557514e2dc5ff2fbb3ad1108574ef
Nila Georgiana Iulia <georgianaiulianila@yahoo.com>
2020-04-15 00:15:30 +0300

    add another interface for search accomodation-not done

---
+++ APLICATIE/app/src/main/java/com/example/infotrip/SearchAccomodationActivity.java
@@ -0,0 +1,12 @@
+package com.example.infotrip;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchAccomodationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_accomodation);
+    }
+}
--- APLICATIE/app/src/main/java/com/example/infotrip/UserProfileActivity.java
+++ APLICATIE/app/src/main/java/com/example/infotrip/UserProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.infotrip;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class UserProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+
+    public void onClickAddPhoto(View view) {
+
+    }
+
+    public void onClickHistory(View view) {
     }
 }
