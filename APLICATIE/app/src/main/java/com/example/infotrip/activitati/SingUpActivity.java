507ae6e76325f76d1a42786b1a9b1ae9cacf6720
Nila Georgiana Iulia <georgianaiulianila@yahoo.com>
2020-06-21 21:58:40 +0300

    add all licenta scrisa without anexe bibliografie+ dezvoltari viitoare

--- APLICATIE/app/src/main/java/com/example/infotrip/activitati/SingUpActivity.java
+++ APLICATIE/app/src/main/java/com/example/infotrip/activitati/SingUpActivity.java
@@ -1,16 +1,28 @@
 package com.example.infotrip.activitati;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.infotrip.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.SignInMethodQueryResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,8 +40,15 @@ import java.util.Arrays;
 
 public class SingUpActivity extends AppCompatActivity {
 
+
+
     Button singUp;
     Spinner spinnerTara;
+    EditText editTextnume,editTextprenume,editTextdata,editTextemail,editTextpass,editTextpass2;
+String nume,prenume,data,tara,email,pass,pass2;
+    private ProgressBar progressDialog;
+    private FirebaseAuth firebaseAuth;
+
 
     //JSON RETEA
     public class  Info extends AsyncTask<String,Void,String>
@@ -109,14 +128,68 @@ public class SingUpActivity extends AppCompatActivity {
         singUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                registerUser();
                 openUserProfilActivity();
             }
         });
+        firebaseAuth=FirebaseAuth.getInstance();
+        progressDialog=(ProgressBar)findViewById(R.id.progressBar);
         spinnerTara=(Spinner)findViewById(R.id.spinnerSingUpActivityTara);
-
+        editTextnume=(EditText)findViewById(R.id.editTextSingUpActivityFirstName);
+        editTextprenume=(EditText)findViewById(R.id.editTextSingUpActivityLastName);
+        editTextdata=(EditText)findViewById(R.id.editTextSingUpActivityBirthDate);
+        editTextemail=(EditText)findViewById(R.id.editTextSingUpActivityEmail);
+        editTextpass=(EditText)findViewById(R.id.editTextSingUpActivityParola);
+        editTextpass2=(EditText)findViewById(R.id.editTextSingUpActivityCheckPass);
 
 
         cauta();
+    }
+
+    private void registerUser() {
+
+
+        if(editTextnume.getText().toString().isEmpty()||
+                editTextprenume.getText().toString().isEmpty()||
+                editTextdata.getText().toString().isEmpty()||
+                editTextpass.getText().toString().isEmpty()||
+                editTextpass2.getText().toString().isEmpty()||
+                editTextemail.getText().toString().isEmpty()||
+                spinnerTara.getSelectedItem().toString().isEmpty()){
+
+            Toast.makeText(getApplicationContext(),"Fill Detail",Toast.LENGTH_LONG).show();
+        }else{
+             nume=editTextnume.getText().toString().trim();
+             prenume=editTextprenume.getText().toString().trim();
+             data=editTextdata.getText().toString().trim();
+             email=editTextemail.getText().toString().trim();
+             pass=editTextpass.getText().toString().trim();
+             pass2=editTextpass2.getText().toString().trim();
+             tara = spinnerTara.getSelectedItem().toString();
+        }
+
+        //if validation are ok
+        //progress bar
+        progressDialog.setVisibility(View.VISIBLE);
+
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                //aici verificari email daca exista si verificare match password si salvare in room a celorlalte detalii in functie de email
+                if(task.isSuccessful()){
+                    Toast.makeText(SingUpActivity.this,"Registered Successfully",Toast.LENGTH_SHORT).show();
+
+
+                }else{
+                    Toast.makeText(SingUpActivity.this,"Could not register, please try again",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
     }
 
     private void openUserProfilActivity() {
